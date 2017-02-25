560175d1a4f5303aa6d1c217adb02c96e4bd77f6
Maiddog <maiddogsrl@gmail.com>
2017-02-25 13:57:08 -0600

    Change definitions and add caching.

--- mod_discord/api_channel.cpp
+++ mod_discord/api_channel.cpp
@@ -7,26 +7,40 @@ namespace ModDiscord
   {
     namespace Channel
     {
-      ModDiscord::Channel get_channel(snowflake channel_id)
+      std::shared_ptr<ModDiscord::Channel> get_channel(snowflake channel_id)
       {
+        if (ChannelCache.count(channel_id))
+        {
+          BOOST_LOG_TRIVIAL(info) << "Returning channel from cache.";
+          return ChannelCache[channel_id];
+        }
+
         auto json = request(GET, "channels/" + std::to_string(channel_id));
 
         if (!json.empty())
         {
-          return ModDiscord::Channel(json);
+          return std::make_shared<ModDiscord::Channel>(json);
         }
         
-        return ModDiscord::Channel();
+        return std::make_shared<ModDiscord::Channel>();
       }
 
-      nlohmann::json create_message(snowflake channel_id, std::string content)
+      std::shared_ptr<Message> create_message(snowflake channel_id, std::string content, bool tts)
       {
-        return request(POST, "channels/" + std::to_string(channel_id) + "/messages", {
+        auto response = request(POST, "channels/" + std::to_string(channel_id) + "/messages", {
           { "content", content },
           { "mentions", nlohmann::json::array() },
-          { "tts", false },
+          { "tts", tts },
           { "embed", nullptr }
         });
+
+        return std::make_shared<Message>(response);
+      }
+
+      bool delete_message(snowflake channel_id, snowflake message_id)
+      {
+        auto response = request(DELETE, "channels/" + std::to_string(channel_id) + "/messages/" + std::to_string(message_id));
+        return response["response_status"].get<int>() == web::http::status_codes::NoContent;
       }
     }
   }
--- mod_discord/api_channel.h
+++ mod_discord/api_channel.h
@@ -9,8 +9,11 @@ namespace ModDiscord
   {
     namespace Channel
     {
-      ModDiscord::Channel get_channel(snowflake channel_id);
-      nlohmann::json create_message(snowflake channel_id, std::string content);
+      static std::map<snowflake, std::shared_ptr<ModDiscord::Channel>> ChannelCache;
+
+      std::shared_ptr<ModDiscord::Channel> get_channel(snowflake channel_id);
+      std::shared_ptr<Message> create_message(snowflake channel_id, std::string content, bool tts = false);
+      bool delete_message(snowflake channel_id, snowflake message_id);
     }
   }
 }
