b07d2494c1dab559c24c95d66249dbc1a2fbb5db
Maiddog <maiddogsrl@gmail.com>
2017-02-25 13:55:29 -0600

    Add missing comma

--- mod_discord/common.h
+++ mod_discord/common.h
@@ -76,7 +76,7 @@ namespace ModDiscord
     template <typename T>
     void set_id_from_json(T key, nlohmann::json data)
     {
-      set_from_json(m_id, key data);
+      set_from_json(m_id, key, data);
     }
 
     void set_id(snowflake id)
