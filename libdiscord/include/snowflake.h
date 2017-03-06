67f07aafd9c3d0190570942c6994080f8fc06c1f
Maiddog <maiddogsrl@gmail.com>
2017-03-06 16:07:40 -0600

    Put Snowflake into the Discord namespace.

--- libdiscord/include/snowflake.h
+++ libdiscord/include/snowflake.h
@@ -5,60 +5,63 @@
 
 #include "external/json.hpp"
 
-class Snowflake
+namespace Discord
 {
-  uint64_t m_id;
-public:
-  Snowflake() : m_id(0) {};
-  Snowflake(uint64_t id) : m_id(id) {};
-
-  explicit Snowflake(const std::string s)
+  class Snowflake
   {
-    m_id = std::stoull(s);
+    uint64_t m_id;
+  public:
+    Snowflake() : m_id(0) {};
+    Snowflake(uint64_t id) : m_id(id) {};
+
+    explicit Snowflake(const std::string s)
+    {
+      m_id = std::stoull(s);
+    }
+
+    bool operator==(const Snowflake& rhs) const
+    {
+      return m_id == rhs.m_id;
+    }
+
+    bool operator!=(const Snowflake& rhs) const
+    {
+      return m_id != rhs.m_id;
+    }
+
+    bool operator<(const Snowflake& rhs) const
+    {
+      return m_id < rhs.m_id;
+    }
+
+    explicit operator std::string() const
+    {
+      return std::to_string(m_id);
+    }
+
+    explicit operator uint64_t() const
+    {
+      return m_id;
+    }
+
+    uint64_t operator()(Snowflake& s) const
+    {
+      return m_id;
+    }
+
+    std::string to_string() const
+    {
+      return std::to_string(m_id);
+    }
+  };
+
+  inline void to_json(nlohmann::json& json, const Snowflake& id)
+  {
+    json = id.to_string();
   }
 
-  bool operator==(const Snowflake& rhs) const
+  inline void from_json(const nlohmann::json& json, Snowflake& id)
   {
-    return m_id == rhs.m_id;
+    id = Snowflake(json.get<std::string>());
   }
-  
-  bool operator!=(const Snowflake& rhs) const
-  {
-    return m_id != rhs.m_id;
-  }
-
-  bool operator<(const Snowflake& rhs) const
-  {
-    return m_id < rhs.m_id;
-  }
-
-  explicit operator std::string() const
-  {
-    return std::to_string(m_id);
-  }
-
-  explicit operator uint64_t() const
-  {
-    return m_id;
-  }
-
-  uint64_t operator()(Snowflake& s) const
-  {
-    return m_id;
-  }
-
-  std::string to_string() const
-  {
-    return std::to_string(m_id);
-  }
-};
-
-inline void to_json(nlohmann::json& json, const Snowflake& id)
-{
-  json = id.to_string();
-}
-
-inline void from_json(const nlohmann::json& json, Snowflake& id)
-{
-  id = Snowflake(json.get<std::string>());
 }
